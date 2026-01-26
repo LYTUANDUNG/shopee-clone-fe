1667ac27ff3c4ad837178431cc935d35a134db4e
minhhien-e <minhhien6634@gmail.com>
2026-01-27 01:24:28 +0700

    feat(ui): implement  checkbox and handle out-of-stock state

---
+++ src/features/cart/components/cartItem/CartItemCheckbox.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { Checkbox } from '@/components/ui/checkbox';
+
+defineProps<{
+  isInStock: boolean;
+}>();
+
+const checked = defineModel<boolean>('checked');
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-full">
+    <Checkbox
+      v-if="isInStock"
+      v-model:checked="checked"
+    />
+    <span v-else class="text-xs text-gray-400 font-medium">Hết hàng</span>
+  </div>
+</template>
