2eb4d4a65c44a3d192ad8b105a6f95bbeac90c15
LYTDUNG <22130054@st.hcmuaf.edu.vn>
2026-01-28 15:48:31 +0700

    Revert "Merge branch 'feature/atom-button' into develop"
    This reverts commit 2686809829f72a4efe48757e75ae40b2eb186663, reversing
    changes made to f7aa15332920df32f9f0e04265f823ff66e1bc5d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                shopee: {
-                    orange: '#ee4d2d',
-                    'orange-dark': '#f05d40',
-                    'orange-light': '#ffedeb',
-                }
-            },
-        },
-    },
-    plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
