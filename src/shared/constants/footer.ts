8023fae1b127c42f97c784a8871fc595620f6504
(Lê Vũ Hùng) <levuhung678@gmail.com>
2026-02-05 17:52:21 +0700

    Create footer.ts

---
+++ src/shared/constants/footer.ts
@@ -0,0 +1,22 @@
+export const FOOTER_CATEGORIES = [
+    {
+        title: 'THỜI TRANG NAM',
+        link: '/Thời-Trang-Nam-cat.11035567',
+        items: [
+            { name: 'Áo Khoác', link: '/Áo-Khoác-cat.11035567.11035568' },
+            { name: 'Áo Vest và Blazer', link: '/Áo-Vest-và-Blazer-cat.11035567.11035572' },
+            { name: 'Áo Hoodie, Áo Len & Áo Nỉ', link: '/Áo-Hoodie-Áo-Len-Áo-Nỉ-cat.11035567.11035578' },
+            { name: 'Quần Jeans', link: '/Quần-Jeans-cat.11035567.11035583' },
+            // ... Hùng thêm tiếp các mục khác từ HTML vào đây nhé
+        ]
+    },
+    {
+        title: 'NHÀ CỬA & ĐỜI SỐNG',
+        link: '/Nhà-Cửa-Đời-Sống-cat.11036670',
+        items: [
+            { name: 'Chăn, Ga, Gối & Nệm', link: '/Chăn-Ga-Gối-Nệm-cat.11036670.11036683' },
+            { name: 'Đồ nội thất', link: '/Đồ-nội-thất-cat.11036670.11036695' },
+        ]
+    },
+    // Thêm đủ 25+ danh mục như trong hình image_cf906a.png
+];
